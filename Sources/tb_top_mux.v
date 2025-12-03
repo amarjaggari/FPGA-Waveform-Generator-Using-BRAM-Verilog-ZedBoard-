20a1a4a40104bd980f5e22ec378ecd96373e9a8a
amarjaggari <149499676+amarjaggari@users.noreply.github.com>
2025-12-03 16:34:48 +0530

    Update and rename top_mux.v to tb_top_mux.v
    Test bench for top module which is multiplxer

---
+++ Sources/tb_top_mux.v
@@ -0,0 +1,21 @@
+`timescale 1 ps / 1 ps
+
+module tb();
+  reg clk;
+  reg rst;
+  reg [1:0] sel;
+  wire [7:0] wave_out;
+  
+  top_mux dut(clk, rst, sel, wave_out);
+  
+  always #5 clk = ~clk;
+  
+  initial begin
+    clk = 0; rst = 1; sel = 0;
+    #10; rst = 0;
+    #10; sel = 1;
+    #10; sel = 2;
+    #10; sel = 3;
+    #10; $finish;
+  end
+endmodule
